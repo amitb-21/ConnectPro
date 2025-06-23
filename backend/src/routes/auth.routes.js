824bfeac0ff806c324d5cef11917dc9e04dbd25a
amitb-21 <amitkumarbehera2104@gmail.com>
2025-06-23 11:10:42 +0530

    some changes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { authenticateJWT } from '../middleware/jwt.js';
-import { registerUser, loginUser, addToHistory, getUserHistory, deleteMeetingFromHistory  } from '../controllers/user.controller.js';
+import { registerUser, loginUser, addToHistory, getUserHistory, deleteMeetingFromHistory } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
 router.post('/register', registerUser);
-router.post("/add_to_activity", addToHistory);
-router.get("/get_all_activity",getUserHistory);
-router.delete('/user/delete_meeting/:id', deleteMeetingFromHistory);
+router.post("/add_to_activity", authenticateJWT, addToHistory);
+router.get("/get_all_activity", authenticateJWT, getUserHistory);
+router.delete('/delete_meeting/:id', authenticateJWT, deleteMeetingFromHistory);
 
 export default router;
